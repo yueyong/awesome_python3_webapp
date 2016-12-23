6308080f8620193bd0e4092f38e6c448006c93f2
YongYue <yueyong@supeq.com>
2016-12-23 18:03:38 +0800

    web server初始化代码

--- www/app.py
+++ www/app.py
@@ -4,21 +4,36 @@
 
 import asyncio
 import logging
+import os
 
 from aiohttp import web
+
+from www import orm
+from jinja2 import Environment, FileSystemLoader
 
 __author__ = "Vic Yue"
 
 logging.basicConfig(level=logging.INFO)
 
 
-async def index(req):
-    return web.Response(body=b"<h1>Awesome python</h1>", content_type="text/html")
+def init_jinjia2(app, **kw):
+    """
+    Init server jinjia2 module
+    :param app: web.Application object.
+    :param kw: var named args.
+    :return:
+    """
+    logging.info("Init jinjia2 ...")
+    options = dict()
+    path = kw.get("path", None)
+    if not path:
+        path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "templates")
+    env = Environment()
 
 
 async def init(loop):
+    await orm.create_connection_pool()
     app = web.Application(loop=loop)
-    app.router.add_route("GET", "/", index)
     host, port = ("127.0.0.1", 9000)
     srv = await loop.create_server(app.make_handler(), host, port)
     logging.info("Server is running at http://%s:%s" % (host, port))
